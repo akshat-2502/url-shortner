1e59c19009c944aabf6fbb0d3a469591085a2ec7
akshat-2502 <khulbeakshat@gmail.com>
2025-06-21 16:52:36 +0530

    nanoid is done

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,5 +1,15 @@
 import express from "express";
 const app = express();
+import { nanoid } from "nanoid";
+
+app.use(express.json());
+app.use(express.urlencoded({ extented: true }));
+
+app.post("/api/create", (req, res) => {
+  const { url } = req.body;
+  console.log(url);
+  res.send(nanoid(7));
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
