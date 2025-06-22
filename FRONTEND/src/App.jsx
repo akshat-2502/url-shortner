3ffddbed7d0e7d9cb9776f97d6702bdb8f0bf6a7
akshat-2502 <khulbeakshat@gmail.com>
2025-06-22 15:49:41 +0530

    basic ui is ready now making the states

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
-  return <div className="bg-pink-500">App</div>;
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 };
 
 export default App;
---
+++ FRONTEND/src/components/UrlForm.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const UrlForm = () => {
+  return (
+    <form className="space-y-4">
+      <div>
+        <label
+          htmlFor="url"
+          className="block font-medium text-sm text-gray-700 mb-1"
+        >
+          Enter Your URL
+        </label>
+        <input
+          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          type="url"
+          id="url"
+          placeholder="https://example.com"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50"
+      >
+        Shorten URL
+      </button>
+
+      {/* {error && (
+          <div className="mt-4 p-3 bg-red-100 text-red-700 rounded-md">
+            {error}
+          </div>
+        )} */}
+
+      {/* {shortUrl && (
+          <div className="mt-6">
+            <h2 className="text-lg font-semibold mb-2">Your shortened URL:</h2>
+            <div className="flex items-center">
+              <input
+                type="text"
+                readOnly
+                value={shortUrl}
+                className="flex-1 p-2 border border-gray-300 rounded-l-md bg-gray-50"
+              />
+               <button
+                onClick={handleCopy}
+                className={`px-4 py-2 rounded-r-md transition-colors duration-200 ${
+                  copied 
+                    ? 'bg-green-500 text-white hover:bg-green-600' 
+                    : 'bg-gray-200 hover:bg-gray-300'
+                }`}
+              >
+                {copied ? 'Copied!' : 'Copy'}
+              </button>
+            </div>
+          </div>
+        )} */}
+    </form>
+  );
+};
+
+export default UrlForm;
