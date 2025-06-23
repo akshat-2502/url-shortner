3c9b01edc2c844ae6d22160c4acaaa1e5ca89e12
akshat-2502 <khulbeakshat@gmail.com>
2025-06-23 14:59:53 +0530

    login to be started

--- BACKEND/src/controller/auth.controller.js
+++ BACKEND/src/controller/auth.controller.js
@@ -1,10 +1,12 @@
+import { cookieOptions } from "../config/config.js";
 import { registerUser } from "../services/auth.service.js";
 import wrapAsync from "../utils/tryCatchWrapper.js";
 
 export const register_user = wrapAsync(async (req, res) => {
   const { name, email, password } = req.body;
   const token = await registerUser(name, email, password);
-  res.status(200).json(token);
+  res.cookie("accessToken", token, cookieOptions);
+  res.status(200).json({ message: "Login Success!" });
 });
 
 export const login_user = wrapAsync(async (req, res) => {
