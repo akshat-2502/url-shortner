4889a1dac1668f38a85728a2b7c81820444913d7
akshat-2502 <khulbeakshat@gmail.com>
2025-06-25 23:19:41 +0530

    commit increase

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -2,10 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "@tanstack/react-router";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>
 );
