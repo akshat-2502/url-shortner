fc7382d118b49b821d85d23758f7481d821c76ae
akshat-2502 <khulbeakshat@gmail.com>
2025-06-26 01:00:17 +0530

    login logiut action created in slice

--- FRONTEND/src/store/slice/authSlice.js
+++ FRONTEND/src/store/slice/authSlice.js
@@ -3,19 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: null,
   isAuthenticated: false,
-  loading: false,
-  error: null,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    clearError: (state) => {
-      state.error = null;
+    login: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
     },
   },
 });
 
-export const { clearError } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
