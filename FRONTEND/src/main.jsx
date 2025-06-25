52efd8d0b2dcbf5e06cf2bdcd49e1b0eedc94dc6
akshat-2502 <khulbeakshat@gmail.com>
2025-06-26 00:56:59 +0530

    redux store is being setup

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -3,11 +3,14 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routing/routeTree.js";
+import store from "./store/store.js";
 export const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
 
 createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </Provider>
 );
