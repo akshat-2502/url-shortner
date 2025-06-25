3b044df07c91fa3dd8a5135ba27698eb44d89118
akshat-2502 <khulbeakshat@gmail.com>
2025-06-26 00:43:41 +0530

    navbar

--- FRONTEND/src/RootLayout.jsx
+++ FRONTEND/src/RootLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import { Outlet } from "@tanstack/react-router";
+import Navbar from "./components/Navbar";
 
 const RootLayout = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
